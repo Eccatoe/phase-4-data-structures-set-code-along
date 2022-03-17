8e8f7379d41d4690b50f9d59765466ab75aac550
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-03-16 21:45:38 -0400

    commit tree 72f5c594c4c84025b92a1782765ee3853f254d14

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,17 @@
-# your code here
+class MySet
+    def initialize(enumerable=[])
+        @hash={}
+        enumerable.each do |value|
+            @hash[value]=true
+        end
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value]=true
+        self
+    end
+end
